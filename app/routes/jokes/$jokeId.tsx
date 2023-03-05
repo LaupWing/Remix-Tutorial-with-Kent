290106c5a942c78f56da9026baa0e5e8f21f392a
LaupWing <loc_strict@hotmail.com>
2023-03-05 10:15:54 +0100

    udpate

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,4 +1,26 @@
+import type { LoaderFunction } from "@remix-run/node"
+import { Joke } from "@prisma/client"
+import { json } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
+import { db } from "~/utils/db.server"
+
+type LoaderData = {
+   joke: Joke | null
+}
+
+export const loader: LoaderFunction = async ({params}) => {
+   const data: LoaderData = {
+      joke: await db.joke.findUnique({ where: {
+         id: params.jokeId
+      } })
+   }
+
+   return json(data)
+}
+
 export default function JokeRoute() {
+   const data = useLoaderData<LoaderData>()
+
    return (
       <div>
          <p>Here's your hilarious joke:</p>
